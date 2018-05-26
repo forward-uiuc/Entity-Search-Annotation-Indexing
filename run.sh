fb921dc96648efce631c077f6447e2508d070e46
Long Pham <ltpham3@illinois.edu>
2018-05-25 23:15:11 -0500

    add more csv files

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 domain=$1
-mkdir /srv/local/ltpham3/esdoc/$domain
-mkdir /srv/local/ltpham3/esdoc/$domain/data
-mkdir /srv/local/ltpham3/esdoc/$domain/log
-# java -cp target/uber-EntityAnnotation-1.0-SNAPSHOT.jar org.forward.entitysearch.ingestion.HTMLDocumentIngestionManager -i input/$domain.csv -o /srv/local/ltpham3/esdoc/$domain/data/ >> /srv/local/ltpham3/esdoc/$domain/log/std.log 2>> /srv/local/ltpham3/esdoc/$domain/log/err.log &
+output=$2
+mkdir $output$domain
+mkdir $output$domain/data
+mkdir $output$domain/log
+java -cp target/uber-EntityAnnotation-1.0-SNAPSHOT.jar org.forward.entitysearch.ingestion.HTMLDocumentIngestionManager -i input/$domain.csv -o $output$domain/data/ >> $output$domain/log/std.log 2>> $output$domain/log/err.log &
