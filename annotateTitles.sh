06a93d7cced4f599bbac116ab59ab06b87de99d6
Long Pham <ltpham3@illinois.edu>
2018-07-05 22:39:01 -0500

    minor change

---
+++ annotateTitles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for file in `ls $1`; do
+    #[ -d "$file" ] || continue
+    echo "$file"
+    echo "$1/$file/data/"
+    echo "$1/$file/serializedTitles"
+    # java -cp target/uber-EntityAnnotation-1.0-SNAPSHOT.jar org.forward.entitysearch.experiment.CreateAnnotatedTitles $1/$file/data/ $1/$file/serializedTitles
+done
